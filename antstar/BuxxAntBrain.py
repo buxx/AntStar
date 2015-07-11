8a09af7cb182368c6bd760cb6e263667e37f2edc
Bastien Sevajol <sevajol.bastien@gmail.com>
2015-07-11 14:25:38 +0200

    rewrite algo

--- antstar/BuxxAntBrain.py
+++ antstar/BuxxAntBrain.py
@@ -11,6 +11,11 @@ class BuxxAntBrain(AntBrain):
         self._memory_since_blocked = []
         self._by_passing = False
         self._distance_when_blocked = None
+        self._last_direction_tested = None
+        self._directions_tested = []
+
+    def get_memory_since_blocked(self):
+        return self._memory_since_blocked
 
     def has_moved(self):
         if self._by_passing:
@@ -50,25 +55,33 @@ class BuxxAntBrain(AntBrain):
         TODO: Very complex here !
         :return:
         """
-        direction_of_home = self._get_direction_of_home()
-        home_position = self._get_home_position()
-        current_position = self._host.get_position()
-        direction_walkable = False
-        directions_tested = []
-        last_direction_tested = direction_of_home
-        while not direction_walkable:
-            if last_direction_tested in directions_tested:
-                slightly_available = slightly[last_direction_tested]
-                try_direction = [dir for dir in slightly_available if dir not in directions_tested][0]
-            else:
-                try_direction = get_nearest_direction(home_position, current_position, slightly[last_direction_tested])
-            if try_direction not in directions_tested:
-                try_position = get_position_with_direction_decal(try_direction, current_position)
-                if try_position not in self._memory_since_blocked and self._feeler.direction_is_free(try_direction):
-                    return direction_modifiers[try_direction]
-                directions_tested.append(try_direction)
+        self._last_direction_tested = self._get_direction_of_home()
+        self._directions_tested = []
 
-            if len(directions_tested) > 7:
+        while True:
+            try_direction = self._get_new_by_pass_try_direction()
+
+            if self._by_pass_direction_is_possible(try_direction):
+                return direction_modifiers[try_direction]
+
+            self._directions_tested.append(try_direction)
+            if len(self._directions_tested) > 7:
                 raise AlreadyWalkedAround()
 
-            last_direction_tested = try_direction
+            self._last_direction_tested = try_direction
+
+    def _get_new_by_pass_try_direction(self):
+        if self._last_direction_tested in self._directions_tested:
+            slightly_available = slightly[self._last_direction_tested]
+            return [direct for direct in slightly_available if direct not in self._directions_tested][0]
+        else:
+            return get_nearest_direction(self._get_home_position(),
+                                         self._host.get_position(),
+                                         slightly[self._last_direction_tested])
+
+    def _by_pass_direction_is_possible(self, direction):
+        if direction not in self._directions_tested:
+                try_position = get_position_with_direction_decal(direction, self._host.get_position())
+                if try_position not in self._memory_since_blocked and self._feeler.direction_is_free(direction):
+                    return True
+        return False
